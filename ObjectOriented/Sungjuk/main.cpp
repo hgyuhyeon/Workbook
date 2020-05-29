16d5ae4cbb6a7bca6163b0276ce377c4e518f9e0
황규현 <57509844+hwgyuhyeon@users.noreply.github.com>
2020-05-29 13:52:57 +0900

    Update main.cpp

--- ObjectOriented/Sungjuk/main.cpp
+++ ObjectOriented/Sungjuk/main.cpp
@@ -2,36 +2,34 @@
 #include "Student.h"
 #include "Subject.h"
 
-void ShowData(const Student& s);
+void main() {
+	Subject sub1[2] = { Subject("컴퓨터", 3, "C"), Subject("현대무용", 2, "A") }; 
+	Subject* sub2[2] = { new Subject(), new Subject("수학", 3, "C") }; 
+	Student st1; 
+	Student st2("홍길동", 2013909845, 2, sub1); 
+	Student* st3 = new Student[2];
 
-int main(void) {
-	Subject sub1("컴퓨터", 3, "C");
-	Subject sub2(sub1);
-	cout << "\n" << "sub1 정보" << "\n";
-	sub1.PrintData();
-	cout << "\n" << "sub2 정보" << "\n";
-	sub2.PrintData();
-	sub2.Modify();
-	//Student st1;
-	Student st2("홍길동", 2013909845, 1, &sub1);
-	Student st3("김성령", 2015909845, 1, &sub2);
-	//st1 = st2;
-	Student st1(st2); //(+)
-	cout << "\n" << "st1 정보" << "\n";
-	st1.PrintData();
-	cout << "\n" << "st2 정보" << "\n";
-	st2.PrintData();
-	st2.Modify();
-	cout << "\n" << "수정된 st2 정보" << "\n";
-	st2.PrintData();
-	st1.PrintData();
-	cout << "\n" << "st3 정보" << "\n";
-	st3.PrintData();
+	cout << "\n" << "sub2[0] 입력" << "\n"; 
+	sub2[0]->InputData();    
+	cout << "\n" << "st1 입력" << "\n"; 
+	st1.InputData();    
+	cout << "\n" << "st3 입력" << "\n"; 
+	for (int i = 0; i < 2; i++)   
+		(st3 + i)->InputData();
 
-	return 0;
-}
-
-void ShowData(const Student& s) {
-	cout << s.m_name << "이 수강한 과목은 총 " << s.m_subnum << "개로 ";
-	cout << "수강한 과목의 평균평점은" << s.m_aveGPA << "입니다.\n";
+	cout << "\n" << "sub1 정보 출력" << "\n"; 
+	Subject::PrintTitle(); 
+	for (int i = 0; i < 2; i++)   
+		sub1[i].PrintData(); 
+	cout << "\n" << "sub2 정보 출력" << "\n"; 
+	Subject::PrintTitle(); 
+	for (int i = 0; i < 2; i++)   
+		sub2[i]->PrintData(); 
+	cout << "\n" << "st1 정보 출력" << "\n"; 
+	st1.PrintData();    
+	cout << "\n" << "st2 정보 출력" << "\n"; 
+	st2.PrintData(); 
+	cout << "\n" << "st3 정보 출력" << "\n"; 
+	for (int i = 0; i < 2; i++)   
+		(st3 + i)->PrintData();
 }
