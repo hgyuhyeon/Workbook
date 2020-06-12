4012b7b26d20a3cb6de640ffa3d04f4fa16159c8
황규현 <57509844+hwgyuhyeon@users.noreply.github.com>
2020-06-12 23:08:10 +0900

    Update main.cpp

--- ObjectOriented/Sungjuk/main.cpp
+++ ObjectOriented/Sungjuk/main.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include "Student.h"
 #include "Subject.h"
+#include "IOInterface.h"
 
-void main() {
-	Subject sub1[2] = { Subject("컴퓨터", 3, "C"), Subject("현대무용", 2, "A") }; 
-	Subject* sub2[2] = { new Subject(), new Subject("수학", 3, "C") }; 
-	Student st1; 
-	Student st2("홍길동", 2013909845, 2, sub1); 
-	Student* st3 = new Student[2];
+int main() {
+	
+	Subject sub("컴퓨터", 3, "C");
+	cout << "-----------------------------\n";
+	//IOInterface Class에서 상속받은 GetData() 호출;
+	cout << "m_data: " << sub.GetData() << endl;
+	//Subject Class에서 재정의된 GetName() 호츨;
+	cout << "교과목 이름: " << sub.GetName() << endl;
+	//IOInterface Class의 GetName() 호출;
+	cout << "부모클래스의 이름: " << sub.IOInterface::GetName() << endl;
+	cout << "-----------------------------\n\n";
+	Student std("홍길동", 2013909845, 1, &sub);
+	cout << "-----------------------------\n";
+	//IOInterface Class에서 상속받은 GetData() 호출;
+	cout << "m_data: " << std.GetData() << endl;
+	//Student Class에서 재정의된 GetName() 호츨;
+	cout << "학생 이름: " << std.GetName() << endl;
+	//IOInterface Class의 GetName() 호출;
+	cout << "부모클래스의 이름: " << std.IOInterface::GetName() << endl;
+	cout << "-----------------------------\n\n";
 
-	cout << "\n" << "sub2[0] 입력" << "\n"; 
-	sub2[0]->InputData();    
-	cout << "\n" << "st1 입력" << "\n"; 
-	st1.InputData();    
-	cout << "\n" << "st3 입력" << "\n"; 
-	for (int i = 0; i < 2; i++)   
-		(st3 + i)->InputData();
-
-	cout << "\n" << "sub1 정보 출력" << "\n"; 
-	Subject::PrintTitle(); 
-	for (int i = 0; i < 2; i++)   
-		sub1[i].PrintData(); 
-	cout << "\n" << "sub2 정보 출력" << "\n"; 
-	Subject::PrintTitle(); 
-	for (int i = 0; i < 2; i++)   
-		sub2[i]->PrintData(); 
-	cout << "\n" << "st1 정보 출력" << "\n"; 
-	st1.PrintData();    
-	cout << "\n" << "st2 정보 출력" << "\n"; 
-	st2.PrintData(); 
-	cout << "\n" << "st3 정보 출력" << "\n"; 
-	for (int i = 0; i < 2; i++)   
-		(st3 + i)->PrintData();
+	return 0;
 }
